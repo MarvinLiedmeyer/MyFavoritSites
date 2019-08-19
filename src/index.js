a041cdb3d48236bfc6aeba2b002cc80e9a0a59f4
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-08-19 13:45:46 +0200

    :sparkles: InterCom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./app.scss";
 
+const numberArray = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+
 (function (tappProject, chayns, window, undefined) {
     'use strict';
 
     let siteList = [];
+    let $addSite = document.querySelector(".addSite");
 
     tappProject.init = function init(data) {
         chayns.ready.then(() => {
@@ -11,7 +14,9 @@ import "./app.scss";
 
             const $search = document.querySelector(".search");
 
+            $addSite.addEventListener('click', () => { _addMessage() });
             $search.addEventListener('input', e => _search(e.target.value));
+            $search.addEventListener('click', e => { e.stopPropagation() });
 
             _getFetchData();
 
@@ -23,9 +28,10 @@ import "./app.scss";
         listItem.className = "listItem";
         let icon = document.createElement("img");
         icon.alt = "icon";
-        icon.src = `https://sub60.tobit.com/l/${siteId}` 
-        icon.className = "icon";
-        icon.onerror = () => document.querySelector(".icon").src = "https://i.ytimg.com/vi/XIMLoLxmTDw/hqdefault.jpg";
+        icon.src = `https://sub60.tobit.com/l/${siteId}`;
+        const indivClass = numberToText(siteId.replace("-", ""));
+        icon.className = `icon ${indivClass}`;
+        icon.onerror = () => document.querySelector(`.${indivClass}`).src = "https://visualpharm.com/assets/403/Location-595b40b65ba036ed117d27da.svg";
         listItem.appendChild(icon);
         let title = document.createElement("h3");
         title.className = "listItemTitle";
@@ -47,12 +53,25 @@ import "./app.scss";
         return listItem;
     }
 
+    function getNumberString(number) {
+        return numberArray[number];
+    }
+
+    function numberToText(number) {
+        let returnString = "";
+        const convertNumber = number.toString();
+        for (let i = 0; i < convertNumber.length; i++) {
+            returnString = returnString.concat(getNumberString(parseInt(convertNumber.charAt(i))));
+        }
+
+        return returnString;
+    }
+
     function _getFetchData() {
         fetch("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50")
             .then((response) => { return response.json() })
             .then((data) => {
                 siteList = data.Data;
-                console.log(siteList);
                 _createList(siteList);
             })
             .catch((err) => { console.log(err) });
@@ -70,6 +89,24 @@ import "./app.scss";
         document.querySelector(".sitelist").innerHTML = null;
         let filterList = siteList.filter(e => e.appstoreName.includes(searchText));
         _createList(filterList);
+    }
+
+    function _addMessage() {
+        // send application 
+        chayns.intercom.sendMessageToPage({
+            text: `${'Neue Seite'}
+                    ${'ID: '} ${chayns.env.user.id}
+                    ${'Name '} ${document.querySelector("input[name='name']").value}
+                    ${'E-mail '} ${document.querySelector("input[name='email']").value}
+                    ${'Adresse '} ${document.querySelector("input[name='adress']").value}
+                    ${'Kommentar '} ${document.querySelector("textarea[name='comment']").value}`
+        })
+        chayns.dialog.alert("","Danke fürs hinzufügen deiner Webseite. Deine Anfrage wird bearbeitet!");
+        document.querySelector("input[name='name']").value = "";
+        document.querySelector("input[name='email']").value = "";
+        document.querySelector("input[name='adress']").value = "";
+        document.querySelector("textarea[name='comment']").value = "";
+
     }
 
 })((window.tappProject = {}), chayns, window);
